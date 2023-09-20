c6a985d5c55f7b75f0dd9389c01c3eaf81cee752
Isabelidesouza <108525789+Isabelidesouza@users.noreply.github.com>
2023-09-20 12:32:15 -0300

    s

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-
+//pom
 function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
   }
   
 
   document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+  //tecla clap
+function tocaSomclap () {
+    document.querySelector('#som_tecla_clap').play();
+  }
+  
+
+  document.querySelector('.tecla_pom').onclick = tocaSomclap;
+
+  //tecla tim
+  function tocaSomtim () {
+    document.querySelector('#som_tecla_tim').play();
+  }
+  
+
+  document.querySelector('.tecla_tim').onclick = tocaSomtim;
+
   
